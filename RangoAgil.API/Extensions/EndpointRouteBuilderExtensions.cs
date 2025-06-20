34f83cf0f0bb6ad16156fe3748be81c21701db37
Guicesars <guicesarsosil@gmail.com>
2025-06-20 13:30:07 -0300

    chore: organização da estrutura da API e criação de métodos de extensão

---
+++ RangoAgil.API/Extensions/EndpointRouteBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using RangoAgil.API.EndpointHandlers;
+
+namespace RangoAgil.API.Extensions;
+public static class EndpointRouteBuilderExtensions
+{
+    public static void RegisterRangosEndpoints(this IEndpointRouteBuilder endpointRouteBuilder)
+    {
+        var rangosEndpoints = endpointRouteBuilder.MapGroup("/rangos");
+        var rangosEndpointsId = rangosEndpoints.MapGroup("/{rangoId:int}");
+
+        rangosEndpoints.MapGet("", RangosHandlers.GetRangosAsync);
+        rangosEndpointsId.MapGet("", RangosHandlers.GetRangoById).WithName("GetRangos");
+        rangosEndpoints.MapPost("", RangosHandlers.CreateRangoAsync);
+        rangosEndpointsId.MapPut("", RangosHandlers.UpdateRangoAsync);
+        rangosEndpointsId.MapDelete("", RangosHandlers.DeleteRangoAsync);
+
+    }
+
+    public static void RegisterIngredientesEndpoints(this IEndpointRouteBuilder endpointRouteBuilder)
+    {
+        var ingredientesEndpoint = endpointRouteBuilder.MapGroup("rangos/{rangoId:int}/ingredientes");
+        ingredientesEndpoint.MapGet("", IngredientesHandlers.GetIngredientesAsync);
+    }
+}
+
